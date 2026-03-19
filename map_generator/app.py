8c58cebca9d54b4648024aa061a466cef276a731
Cristopher <129361337+CrisVA24@users.noreply.github.com>
2026-03-19 17:36:04 -0600

    Finish Start Screen module

--- map_generator/app.py
+++ map_generator/app.py
@@ -6,9 +6,15 @@ class App:
     def __init__(self):
         self.root = Tk()
         self.root.title('Treasure Trace')
-        self.root.geometry('500x405')
-        self.current_screen = StartScreen(self.root)
+        self.current_screen = StartScreen(self.root, self)
 
     def run(self):
         self.root.mainloop()
+    '''
+    def switch_screen(self, new_screen_class):
+        if self.current_screen:
+            self.current_screen.destroy()
+
+        self.current_screen = new_screen_class(self.root, self)
+    '''
 
--- map_generator/views/start_screen.py
+++ map_generator/views/start_screen.py
@@ -1,7 +1,46 @@
+from tkinter import *
+
 class StartScreen:
 
     def __init__(self, root):
         self.root = root
 
+        #VARIABLES
+        self.size_var = StringVar(value='None')
+
+        #TITLE
+        self.title = Label(root, text='Treasure Trace', fg='black', font=('Helvetica', 20, 'bold'))
+        self.title.pack(pady=10)
+
+        #MAIN CONTAINER
+        self.main_container = Frame(root)
+        self.main_container.pack(pady=(0, 10))
+
+        #INSTRUCTIONS
+        self.instructions = Text(self.main_container, height=14, width=47, padx=20, pady=20)
+        self.instructions.pack(side=LEFT, padx=10)
+        self.instructions.insert(INSERT, "Welcome!\n\nUncover the secrets of ancient civilizations by\ndesigning your own mysterious maps. Place\nobstacles and hide treasures, then watch as\nour expert explorer reveals the quickest way to\nthe gold.\n\nSelect a map size from the options below to\nbuild your custom labyrinth with obstacles,\ntreasures, and a starting point. Once you save\nyour creation, the explorer will unlock and\nautomatically reveal the shortest path to the\nriches.")
+        self.instructions.config(state=DISABLED)
+
+        #SIZE BUTTONS CONTAINER
+        self.button_container = Frame(self.main_container)
+        self.button_container.pack(side=LEFT, padx=(0, 10))
+
+        #SIZE BUTTONS
+        options = ["Small", "Medium", "Large"]
         
-        
+        for item in options:
+            button = Radiobutton(self.button_container,
+                              text=item,
+                              variable=self.size_var,
+                              value=item,
+                              indicatoron=0,
+                              width=10,
+                              font=('Helvetica', 12),
+                              selectcolor="#d1d1d1",
+                              bg='#ffffff')
+            button.pack(pady=10)        
+
+        #START BUTTON
+        self.start_button = Button(root, width=20, text='Start Adventure', font=('Helvetica', 18, 'bold'), bg='#ffffff',fg='black')
+        self.start_button.pack(pady=(0, 10))
